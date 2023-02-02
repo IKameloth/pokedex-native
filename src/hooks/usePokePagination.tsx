d7c7400858f1b67cf2a529aa250755fd74f97984
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2023-02-02 09:21:58 -0300

    :sparkles: custom hook with api call (axios)

---
+++ src/hooks/usePokePagination.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
+import { pokemonApi } from '../api/pokemonApi'
+import { PokemonResponse, Result, SimplePokemon } from '../interfaces/pokemonInterface'
+
+export const usePokePagination = () => {
+  const [isLoading, setIsLoading] = useState<Boolean>(true)
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
+  const nextPageRef = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
+
+  const loadPokemons = async () => {
+    setIsLoading(true)
+    const response = await pokemonApi.get<PokemonResponse>(nextPageRef.current)
+    nextPageRef.current = response.data.next
+    mapPokemonList(response.data.results)
+  }
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    // pokemonList.forEach((pokemon) => console.log(pokemon.name))
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+      const urlPart = url.split('/')
+      const id = urlPart[urlPart.length - 2]
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+      return {
+        id,
+        picture,
+        name
+      }
+    })
+    setSimplePokemonList([...simplePokemonList, ...newPokemonList])
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    loadPokemons()
+  }, [])
+
+  return { isLoading, simplePokemonList }
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokePagination } from '../hooks/usePokePagination'
+import { styles } from '../theme/appTheme'
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets()
+  const { isLoading, simplePokemonList } = usePokePagination()
+  console.log(isLoading, { simplePokemonList })
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image source={require('../assets/pokeball.png')} style={styles.pokeballBG} />
+      <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20 }}>Pokedex</Text>
+    </>
   )
 }
